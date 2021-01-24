06ce9a6e91b399cc8f1f46c53fb3066f9172388c
Muhamad Irvan Dandung <irvandandung1@gmail.com>
2021-01-24 11:52:26 +0700

    add progruad conf & more security in more object

--- core/src/main/java/project/irvandandung/bioskopmovie/core/di/CoreModule.kt
+++ core/src/main/java/project/irvandandung/bioskopmovie/core/di/CoreModule.kt
@@ -1,6 +1,9 @@
 package project.irvandandung.bioskopmovie.core.di
 
 import androidx.room.Room
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,28 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDB>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dandoeng".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDB::class.java, "movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
